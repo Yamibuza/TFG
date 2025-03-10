e7f5545b361dbe7db834570ec8a47285857ee287
Jose Vicente Aybar <joseviaybar@gmail.com>
2025-03-10 12:30:54 +0100

    Manejando rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LobbyComponent } from './lobby/lobby.component';
+import { FooterComponent } from './footer/footer.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+  { path: '', component: LobbyComponent },
+  { path: 'foot', component: FooterComponent },
+
+];
